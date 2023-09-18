82829ceceb716fa4ba901642a936a9b6c87bd810
Ziad Tarek <ziadtarekfa@gmail.com>
2023-09-18 16:33:26 +0300

    set up tailwindcss

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <h1 className='m-2 bg-red-800 text-2xl text-cyan-700'>HELLO WORLD</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
 
-app.listen(5000, () => {
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+io.on("connection", (socket) => {
+    // ...
+});
+httpServer.listen(5000, () => {
     console.log("Server is now listening");
 })
