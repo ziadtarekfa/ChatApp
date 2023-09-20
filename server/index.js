973477ec645005041a4f5287943b5a56d6b93ad7
Ziad Tarek <ziadtarekfa@gmail.com>
2023-09-20 23:12:37 +0300

    set up mongoose and env file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
+import http from "http";
+import { configDotenv } from "dotenv";
 
+configDotenv();
+
+const mongoURI = process.env.MONGO_URI;
 const app = express();
-const httpServer = createServer(app);
+const httpServer = http.createServer(app);
+
+mongoose.connect(mongoURI).then(() => {
+    console.log("Connected to MongoDB!");
+    httpServer.listen(5000, () => {
+        console.log("Server is now listening on port 5000");
+    });
+}).catch((error) => {
+    console.log("Error connecting to database: " + error);
+})
 
 const io = new Server(httpServer, {
     cors: {
@@ -10,9 +25,7 @@ const io = new Server(httpServer, {
     }
 })
 
+
 io.on("connection", (socket) => {
     // ...
 });
-httpServer.listen(5000, () => {
-    console.log("Server is now listening");
-})
